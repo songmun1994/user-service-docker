42e015f53c568c7bb625bbe6ee87c0c9a1da5e06
songmun1994 <songm7880@naver.com>
2026-04-07 23:06:28 +0900

    test: add star tree print test and update Jenkinsfile

--- src/test/java/com/studyproject/userservicedocker/UserServiceDockerApplicationTests.java
+++ src/test/java/com/studyproject/userservicedocker/UserServiceDockerApplicationTests.java
@@ -7,7 +7,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 class UserServiceDockerApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void printStarTreeTest() {
+        System.out.println("======= Jenkins Test Stage: Star Tree =======");
+        int rows = 5;
 
+        for (int i = 1; i <= rows; i++) {
+            // 공백 출력
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("=============================================");
+    }
 }
